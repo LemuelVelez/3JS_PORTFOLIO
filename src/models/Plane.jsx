fce2681cc6bb6c58dc59185d6db5a79ffb09fb6a
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-04 00:22:22 +0800

    add useEffect in Plane

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,10 +1,15 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import planeScene from "../assets/3d/plane.glb";
 import { useAnimations, useGLTF } from "@react-three/drei";
 const Plane = ({ isRotating, ...props }) => {
   const ref = useRef();
   const { scene, animations } = useGLTF(planeScene);
   const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    
+  }, [isRotating, actions]);
+  
   return (
     <mesh {...props} ref={ref}>
       <primitive object={scene} />
