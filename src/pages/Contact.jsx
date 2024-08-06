fa4859dc1ae520ff5df6ce909dbbe696994f8aeb
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-06 22:47:48 +0800

    add section, div, h1

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="relative flex lg:flex-rowq flex-col max-container">
+      <div>
+        <h1>Get in Touch</h1>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
