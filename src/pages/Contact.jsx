93fc5831cb2ea4315c1c8f24d129bd3b9d9a7d9b
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-06 22:54:00 +0800

    add form, label, input, useState

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
   return (
     <section className="relative flex lg:flex-rowq flex-col max-container">
-      <div>
-        <h1>Get in Touch</h1>
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="John"
+              className="input"
+              required
+            />
+          </label>
+        </form>
       </div>
     </section>
   );
