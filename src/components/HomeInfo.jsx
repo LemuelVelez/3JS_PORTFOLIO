b1b49564d05387ab5ccd75e8a20fbf3d41079a2f
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-06 22:40:00 +0800

    done HomeInfo popups

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,7 +7,7 @@ const InfoBox = ({ text, link, btnText }) => (
     <p className="font-medium sm:text-xl text-center">{text}</p>
     <Link to={link} className="neo-brutalism-white neo-btn">
       {btnText}
-      <img src={arrow} />
+      <img src={arrow} className="w-4 h-4 object-contain" />
     </Link>
   </div>
 );
@@ -26,8 +26,20 @@ const renderContent = {
       btnText="Learn More"
     />
   ),
-  3: <h1>3</h1>,
-  4: <h1>4</h1>,
+  3: (
+    <InfoBox
+      text=" I am currently working on my portfolio. I have some projects that I have done in my time. I am open to new opportunities and would love to connect with you!"
+      link="/projects"
+      btnText="Visit Projects"
+    />
+  ),
+  4: (
+    <InfoBox
+      text=" I am always looking for new opportunities to work on. If you like what you see and want to work together, feel free to contact me. I would love to hear from you!"
+      link="/contact"
+      btnText="Let's get in touch"
+    />
+  ),
 };
 
 const HomeInfo = ({ currentStage }) => {
