88c2f00e8b4870ffa0842c465b281689d5996d39
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-07 00:24:40 +0800

    done for emailjs send message, it works!

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,25 +13,30 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.send(
-      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: form.name,
-        to_name: "Lemuel",
-        from_email: form.email,
-        to_email: "velezlem12@gmail.com",
-        message: form.message,
-      },
-      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    ).then(() => {
-      setIsLoading(false);
-      alert("Thank you. I will get back to you as soon as possible");
-      setForm({ name: "", email: "", message: "" });
-    }).catch((error) => {
-      setIsLoading(false);
-      alert("Something went wrong. Please try again");
-    });
+    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Lemuel",
+          from_email: form.email,
+          to_email: "velezlem12@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        alert("Thank you. I will get back to you as soon as possible");
+        setForm({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+        alert("Something went wrong. Please try again");
+      });
   };
   const handleFocus = () => {};
   const handleBlur = () => {};
