9489e0fbe7c28c1bfb7c83d83bc4b7fa0e8365f5
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-06 23:08:52 +0800

    add handle Submit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,13 +7,17 @@ const Contact = () => {
   const handleChange = () => {};
   const handleFocus = () => {};
   const handleBlur = () => {};
+  const handleSubmit = () => {};
 
   return (
     <section className="relative flex lg:flex-rowq flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
           <label className="text-black-500 font-semibold">
             Name
             <input
