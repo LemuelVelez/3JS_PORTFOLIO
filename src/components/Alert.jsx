1b5c174785b83e115903ee033d98d24eab69e8da
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-20 01:11:58 +0800

    add divs

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const Alert = () => {
+const Alert = ({ type, text }) => {
   return (
-    <div>Alert</div>
-  )
-}
+    <div className="absolute top-0 left-0 right-0 flex justify-center items-center">
+      <div
+        className={`${
+          type === "danger" ? "bg-red-500" : "bg-blue-800"
+        }p-2 text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex`}
+        role="alert"
+      >
+        test
+      </div>
+    </div>
+  );
+};
 
-export default Alert
+export default Alert;
