53f7c1d1e07a2a9c25d45979c93433bd831bff8e
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-19 21:53:20 +0800

    add useState

---
+++ src/hooks/useAlert.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const useAlert = () => {
+    const [alert, setAlert] = useState({
+        show: false, text: "", type: "danger"
+    })
+
+    const showAlert = (text, type = "danger") => setAlert({
+        show: true,
+        text,
+        type
+    })
+    return (
+        <div>useAlert</div>
+    )
+}
+
+export default useAlert
