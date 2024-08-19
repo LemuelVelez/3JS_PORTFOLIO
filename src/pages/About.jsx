e4dafb779837cd739908a6f233fe929394bdd254
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-20 02:28:18 +0800

    feat: div and p

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,22 @@ import React from "react";
 const About = () => {
   return (
     <section className="max-container">
-      <h1 className="head-text">Hello</h1>
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Lemuel.
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Web Developer based in the Philippines, specializing in Frontend
+          development.
+        </p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+      </div>
     </section>
   );
 };
