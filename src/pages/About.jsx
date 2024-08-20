a02f644e66d29fafbd09ad0e0253c552d15c1ecf
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-21 03:21:16 +0800

    feat: about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -57,7 +57,26 @@ const About = () => {
         <div className="mt-12 flex">
           <VerticalTimeline>
             {experiences.map((experience) => (
-              <VerticalTimelineElement>
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: "none",
+                }}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
+                    <img
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className="w-[60%] h-[60%] object-contain"
+                    />
+                  </div>
+                }
+                iconStyle={{ background: experience.iconBg }}
+              >
                 <div>
                   <h3 className="text-black text-xl font-poppins font-semibold">
                     {experience.title}
@@ -69,6 +88,16 @@ const About = () => {
                     {experience.company_name}
                   </p>
                 </div>
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className="text-black-500/50 font-normal pl-1 text-sm"
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
               </VerticalTimelineElement>
             ))}
           </VerticalTimeline>
