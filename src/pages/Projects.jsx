6afc6059a35a065e4c77329556c0842bac04c2e7
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-21 17:33:41 +0800

    feat: few changes. fix: project nav

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Lemuel.
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Web Developer based in the Philippines, specializing in Frontend
+          development.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
