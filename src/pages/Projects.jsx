bd8866208f0f18a8ab5eff30c2a2472df150ecc2
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-21 18:14:52 +0800

    feat: done projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { projects } from "../constants";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+import CTA from "../components/CTA";
 
 const Projects = () => {
   return (
@@ -24,17 +27,43 @@ const Projects = () => {
       <div className="flex flex-wrap my-20 gap-16">
         {projects.map((project) => (
           <div className="lg-w-[400px] w-full" key={project.name}>
-            <div className={`btn-back rounded-xl ${project.theme}`} />
-            <div>
-              <img
-                src={project.iconUrl}
-                alt="Project Icon"
-                className="w-1/2 h-1/2 object-contain"
-              />
+            <div className="block-container w-12 h-12">
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={project.iconUrl}
+                  alt="Project Icon"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-poppins font-semibold">
+                {project.name}
+              </h4>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex items-center gap-2 font-poppins">
+                <Link
+                  to={project.link}
+                  target="_blank"
+                  className="font-semibold text-blue-600"
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt="arrow"
+                  className="w-4 h-4 object-contain"
+                />
+              </div>
             </div>
           </div>
         ))}
       </div>
+
+      <hr className="border-slate-200" />
+
+      <CTA />
     </section>
   );
 };
