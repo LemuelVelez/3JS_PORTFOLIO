a51150f681e923287fe12830ca1ae2047e38c8cc
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-21 18:32:06 +0800

    feat: done sound

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import Plane from "../models/Plane";
 import HomeInfo from "../components/HomeInfo";
 
 import sakura from "../assets/sakura.mp3";
+import { soundoff, soundon } from "../assets/icons";
 const Home = () => {
   const audioRef = useRef(new Audio(sakura));
   audioRef.current.volume = 0.4;
@@ -109,6 +110,14 @@ const Home = () => {
           />
         </Suspense>
       </Canvas>
+
+      <div className="absolute bottom-2 left-2">
+        <img src={!isPlayingMusic ? soundoff : soundon}
+        alt="sound"
+        className="w-10 h-10 cursor-pointer object-contain"
+        onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+         />
+      </div>
     </section>
   );
 };
